6c93cb1524726badc1fd69f549f71f9249804326
Hank Shen <swh@admpub.com>
2023-03-12 18:42:39 +0800

    update

--- autoload/autoload.go
+++ autoload/autoload.go
@@ -1,13 +1,5 @@
 package autoload
 
-/*
-	You can just read the .env file on import just by doing
-
-		import _ "github.com/admpub/godotenv/autoload"
-
-	And bob's your mother's brother
-*/
-
 import "github.com/admpub/godotenv"
 
 func init() {
